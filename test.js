85c8336761bcc184b359cf9a5480d33cc77bc5de
Victor Gonzalez <victor.gonzalezro@gmail.com>
2016-05-19 22:07:36 -0400

    remove unused code
    Signed-off-by: Victor Gonzalez <victor.gonzalezro@gmail.com>

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 var smartCitizen = require("./")
 var smart = smartCitizen("2cdea7d41ccd90ad377196d1fb02fb0f867cdc0b");
-//a.setApiKey();
+
 smart.measurements.listAll(function(err, data){
   if(err){
     console.log("Error");
